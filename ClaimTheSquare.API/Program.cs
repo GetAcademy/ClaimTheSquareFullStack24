79f3f02c1e71b9f8dec5bdd094d2e7d3fab20fe3
GET Academy <terje@kolderup.net>
2024-12-12 13:45:22 +0100

    Update Program.cs

--- ClaimTheSquare.API/Program.cs
+++ ClaimTheSquare.API/Program.cs
@@ -6,17 +6,20 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+var connStr = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ClaimTheSquare;Integrated Security=True";
 app.MapGet("/textobjects", async () =>
 {
-    var connStr = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=TextObjects;Integrated Security=True";
     var sql = "SELECT * FROM TextObject";
     var conn = new SqlConnection(connStr);
     var textObjects = await conn.QueryAsync<TextObject>(sql);
     return textObjects;
 });
-app.MapPost("/textobjects", (TextObject textObject) =>
+app.MapPost("/textobjects", async (TextObject textObject) =>
 {
-    //textObjects.Add(textObject);
+    var sql = "INSERT INTO TextObject VALUES (@Index, @Text, @ForeColor, @BackColor)";
+    var conn = new SqlConnection(connStr);
+    var rowsAffected = await conn.ExecuteAsync(sql, textObject);
+    return rowsAffected;
 });
 app.Run();
 
